231efc199c04b25df6a7ccb215b9d016e4ac1e77
mortuie <jimmybob-leon@hotmail.co.uk>
2019-12-19 18:30:05 +0000

    Adding more initial installs

--- install.sh
+++ install.sh
@@ -6,13 +6,15 @@ set -e
 echo "Installing some important packages through apt";
 sudo apt -y update \
   && sudo apt -y upgrade \
-  && sudo apt install tmux curl wget zsh vim
-
+  && sudo apt install tmux curl wget zsh vim zlib1g-dev libffi-dev \
+  libssl-dev libreadline-dev bzip2 make build-essential libsqlite3-dev \
+  xz-utils apt-transport-https ca-certificates software-properties-common
 
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm -rf ~/.zshrc
 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 
 for i in vim vimrc viminfo zshrc aliases functions tmux.conf gitignore_global;
@@ -22,4 +24,5 @@ do
 done
 
 
+echo "Manual changes needed for KDE - Workspaces";
 echo "Please relog into your account for the full changes to take affect.";
