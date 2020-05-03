e8e08c59fdfe3669f008f8f61c82fc665eb757d4
mortuie <jimmybob-leon@hotmail.co.uk>
2020-05-03 01:43:04 +0100

    Adding nvm install and some comments

--- install.sh
+++ install.sh
@@ -11,18 +11,26 @@ sudo apt -y update \
   xz-utils apt-transport-https ca-certificates software-properties-common
 
 
+# installing all init files
 for i in vim vimrc viminfo zshrc aliases functions tmux.conf gitignore_global;
 do	
 	echo "Installing your: $i";
 	ln -sf ~/.dotfiles/$i ~/.$i
 done
 
+
+# installs vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/vim/bundle/Vundle.vim
 
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# installs zsh + oh my zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm -rf ~/.zshrc
 
+# installing pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 echo "Manual changes needed for KDE - Workspaces";
